4b52d63287bb9fc57c4dbe662911d4642b256d7c
Siladittya Manna <sadimanna@users.noreply.github.com>
2024-09-05 17:33:20 +0530

    Update vision_transformer.py

--- dinov2/models/vision_transformer.py
+++ dinov2/models/vision_transformer.py
@@ -17,7 +17,7 @@ import torch.nn as nn
 import torch.utils.checkpoint
 from torch.nn.init import trunc_normal_
 
-from dinov2.layers import Mlp, PatchEmbed, SwiGLUFFNFused, MemEffAttention, NestedTensorBlock as Block
+from layers import Mlp, PatchEmbed, SwiGLUFFNFused, MemEffAttention, NestedTensorBlock as Block
 
 
 logger = logging.getLogger("dinov2")
